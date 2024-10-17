e1494ac78b99e94a4d90c40a9f28558da863eb28
Sriharish <harish872004@gmail.com>
2024-10-18 00:58:09 +0530

    env change

--- index.js
+++ index.js
@@ -2,66 +2,66 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
-
 const dotenv = require("dotenv").config();
 const URL = process.env.DB;
 
 const DB_NAME = "movie_db";
+
 const COLLECTION_NAME = "movies";
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(cors({
-    origin : "*"
-}));
+app.get("/movie/get-movies", async (req, res) => {
+  try {
+    //  Connect the Database
+    const client = new MongoClient(URL, {}).connect();
 
+    // Step 2. Select the Database
+    let db = (await client).db(DB_NAME);
 
+    // Select the Collection
+    let collection = await db.collection(COLLECTION_NAME);
 
-app.get("/movie/get-movies", async (req,res) => {
+    // Do the operation
+    let movies = await collection.find({}).toArray();
 
-    try{
-            // connect the database
-            const client = new MongoClient(URL,{}).connect();
-            // select the db
-            let db = (await client).db(DB_NAME)
-            //select the collection
-            let collection = await db.collection(COLLECTION_NAME);
-            //do the operation 
-            let movies = await collection.find({}).toArray();
-            //close the connection
-          (await client).close();
+    // Close the connection
+    (await client).close();
 
- res.json(movies);
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
-});
-
-app.get("movie/:id", async (req,res) => {
-    
-    try{
-        const id = req.params.id;
-        // connect the database
-        const client = new MongoClient(URL,{}).connect();
-        // select the db
-        let db = (await client).db(DB_NAME)
-        //select the collection
-        let dbcollection = await db.collection(COLLECTION_NAME);
-        //do the operation 
-        let movie = await dbcollection.findOne({ _id : new ObjectId(id) });
-        //close the connection
-        (await client).close();
-
-        res.json(movie);
-}
-catch(error){
+    res.json(movies);
+  } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
-}
+  }
 });
 
-app.post("book-ticket",(req,res) => {
+app.get("/movie/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
+    // Connect the Database
+    const client = new MongoClient(URL, {}).connect();
+
+    // Select the Database
+    let db = (await client).db(DB_NAME);
+
+    // Select the Collection
+    let dbcollection = await db.collection(COLLECTION_NAME);
+     // Do the operation
+    let movie = await dbcollection.findOne({ _id: new ObjectId(id) });
+     // Close the connection
+    (await client).close();
+
+    res.json(movie);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
-app.listen(4000);
+app.post("/book-ticket", (req, res) => {});
+
+app.listen(8000);
