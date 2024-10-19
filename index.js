4800326c11faefa197656a30c4901039952b640f
Sriharish <harish872004@gmail.com>
2024-10-19 09:00:33 +0530

    change3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
 
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const URL = process.env.DB;
 
 const DB_NAME = "movie_db";
@@ -48,7 +48,7 @@ app.get("/movie/:id", async (req,res) => {
         //select the collection
         let dbcollection = await db.collection(COLLECTION_NAME);
         //do the operation 
-        let movie = await dbcollection.findOne({ _id : new ObjectId(id) });
+        let movie = await dbcollection.findOne({ _id: new ObjectId(id) });
         //close the connection
         (await client).close();
 
@@ -60,8 +60,106 @@ catch(error){
 }
 });
 
-app.post("/book-ticket", (req,res) => {
+app.post("/movie/book-movie", async (req, res) => {
+    let bookingRequest = req.body;
+  
+    if (
+      !bookingRequest.movieId ||
+      !bookingRequest.showId ||
+      !bookingRequest.seats ||
+      !bookingRequest.name ||
+      !bookingRequest.email ||
+      !bookingRequest.phoneNumber
+    ) {
+      return res.status(401).json({ message: "Some fields are missing" });
+    }
+    let requestedSeat = parseInt(bookingRequest.seats);
 
-});
-
-app.listen(4000);
+    if (isNaN(requestedSeat) || requestedSeat <= 0) {
+      return res.status(401).json({ message: "In valid seat count" });
+    }
+  
+    try {
+      // Connect the Database
+      const client = new MongoClient(URL, {}).connect();
+  
+      // Select the DB
+      let db = (await client).db(DB_NAME);
+  
+      // Select the Collection
+      let dbcollection = await db.collection(COLLECTION_NAME);
+  
+      /**
+       * Find the movie
+       * if movie is not found throw error else
+       * check if the seats are available
+       * Find the show and get the seat
+       * If the available seat is less than requested seat a:10 r:11
+       * Throw error
+       * Else book the seat
+       */
+      console.log(bookingRequest.movieId);
+      let movie = await dbcollection.findOne({
+        _id: new ObjectId(bookingRequest.movieId),
+      });
+  
+      if (!movie) {
+        return res.status(404).json({ message: "Requested movie is not found" });
+      }
+  
+      const show = Object.values(movie.shows)
+        .flat()
+        .find((s) => s.id === bookingRequest.showId);
+  
+      if (!show) {
+        return res.status(404).json({ message: "Show not Found" });
+      }
+  
+      if (parseInt(show.seats) < requestedSeat) {
+        return res.status(404).json({ message: "No enough seats avilable" });
+      }
+  
+      const updateSeats = parseInt(show.seats) - requestedSeat;
+  
+      const date = Object.keys(movie.shows).find((d) =>
+        movie.shows[d].some((s) => s.id === bookingRequest.showId)
+      );
+      console.log(movie.shows[date]);
+      const showIndex = movie.shows[date].findIndex(
+        (s) => s.id === bookingRequest.showId
+      );
+      console.log(showIndex);
+  
+      const userBooking = {
+        name: bookingRequest.name,
+        email: bookingRequest.email,
+        phoneNumber: bookingRequest.phoneNumber,
+        seats: bookingRequest.seats,
+      };
+  
+      const updatedResult = await dbcollection.updateOne(
+        {
+          _id: new ObjectId(bookingRequest.movieId),
+        },
+        {
+          $set: {
+            [`shows.${date}.${showIndex}.seats`]: updateSeats,
+          },
+          $push: {
+            [`shows.${date}.${showIndex}.bookings`]: userBooking,
+          },
+        }
+      );
+  
+      if (updatedResult.modifiedCount === 0) {
+        return res.status(500).json({ message: "Failed to update" });
+      }
+  
+      return res.status(200).json({ message: "Booking created successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  });
+  
+  app.listen(8000);
